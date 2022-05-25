ab7e9eb6831bc818579dd6a08f076cf1c99d48cb
Yeonha Jo <yeonhajo65@gamil.com>
2022-05-26 01:15:37 +0900

    스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,34 +9,27 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {  //애플리케이션의 실제 동작에 필요한 구현 객체를 생성, 생성자를 통해서 주입(연결) 한다.
 
-    /**
-     *
-     * 명확한 역할 설명, 코드 중복(new memberRepository()) 제거로 리팩터링
-     */
-/*    public  MemberService memberService(){
-        return new MemberServiceImpl(new memberRepository());
-    }*/
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-/*    public OrderService orderService(){
-        return new OderServiceImpl(new memberRepository(),new discountPolicy());
-    }*/
+
+    @Bean
     public OrderService orderService(){
         return new OderServiceImpl(memberRepository(),discountPolicy());
     }
 
-   /* public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
-    }  */
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }     // 할인 정책 변경
