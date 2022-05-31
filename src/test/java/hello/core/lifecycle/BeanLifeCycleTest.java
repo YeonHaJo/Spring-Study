d86e6d3658b7166601d1c51d21ca38f3afa86879
Yeonha Jo <yeonhajo65@gmail.com>
2022-06-01 04:12:31 +0900

    빈 등록 초기화, 소멸 메서드

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean(initMethod = "init", destroyMethod = "close")
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();//여기까지  테스트가 수행 될때까지 url 정보는 없다.(단일책임원칙)
+            networkClient.setUrl("http://hello_spring.dev");//초기화되는 시점
+            return networkClient;
+        }
+    }
+}
